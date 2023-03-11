c1a09c701c30af3f4d5206f565e0025aab9c92c9
bhuvan <bhuvanchau@gmail.com>
2023-03-10 23:42:49 -0500

    added web api

---
+++ frontend/src/components/Artists.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import spotifyApi from '../../services/SpotifyService'
+const Artists = () => {
+
+
+    
+  return (
+    <div>Artists</div>
+  )
+}
+
+export default Artists
---
+++ frontend/src/services/SpotifyService.js
@@ -0,0 +1,7 @@
+var Spotify = require('spotify-web-api-js');
+var spotifyApi = new SpotifyWebApi();
+spotifyApi.setAccessToken('5791e72126494efa9ca3037e8cc03591');
+
+
+
+export default {spotifyApi}
